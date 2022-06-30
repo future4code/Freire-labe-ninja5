f0fde6a45bb1d91cb4f0aaaf7cedb1e13f7a132a
joaoVictorVictoy <joaovvag@gmail.com>
2022-06-30 12:13:29 -0300

    salvando maria kkk

--- src/components/Contratante/FiltrosContratante.js
+++ src/components/Contratante/FiltrosContratante.js
@@ -2,87 +2,81 @@ import React from 'react';
 import { DivFiltro, DivGrade } from './styled';
 import Servicos from './Servicos';
 
+import AXIOS from 'axios';
+export default class Contratante extends React.Component {
+    state = {
+        Jobs: []
+            
+    }
 
-export default class Contratante extends React.Component{
-    render(){
+    componentDidMount() {
+        this.getAllJobs();
+    }
+
+    getAllJobs = () =>
+        AXIOS
+            .get(
+                'https://labeninjas.herokuapp.com/jobs',
+                {
+                    headers: {
+                        Authorization: '82842cb7-be45-48c9-b492-20aa550cecd1'
+                    }
+                }
+            )
+            .then((response) => {
+                console.log(response.data);
+        
+            }).catch((err) => {
+                alert(err.message);
+                console.log(err.message);
+            });
+
+
+
+
+    render() {
+        const title = this.state.Jobs.map((job) => {
+            return <p>{job.title}</p>;
+        })
+
+        // const price = this.state.Jobs.map((job) => {
+        //     return <p>{job.price}</p>;
+        // })
+        //     const dueDate = this.state.Jobs.map((job) =>{
+        //         return <p>{job.dueDate}</p>;
+        //     })
+
         return (
             <div>
                 <DivFiltro>
-                <input type="number" placeholder="Valor Mínimo"  ></input>
-                <input type="number" placeholder="Valor Máximo"  ></input>
-                <input type="text" placeholder="Busca por Título"  ></input>
-                <select >
-                <option> -- </option>    
-                <option>Menor Valor</option>    
-                <option>Maior Valor</option>    
-                <option>Titulo</option>    
-                <option>Prazo</option>    
-                </select>
+                    <input type="number" placeholder="Valor Mínimo"  ></input>
+                    <input type="number" placeholder="Valor Máximo"  ></input>
+                    <input type="text" placeholder="Busca por Título"  ></input>
+                    <select >
+                        <option> -- </option>
+                        <option>Menor Valor</option>
+                        <option>Maior Valor</option>
+                        <option>Titulo</option>
+                        <option>Prazo</option>
+                    </select>
                 </DivFiltro>
-               <DivGrade>
-               <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
-                <Servicos
-                titulo="Aleatorio"
-                preco="R$100,00"
-                prazo="21/09/2022"
-                />
+                <section>
+                {title}
+                </section>
 
 
-               </DivGrade>
+                <DivGrade>
+
+                    <Servicos
+                        titulo=""
+                        preco=""
+                        prazo=""
+                    />
+
+
+
+
+                </DivGrade>
             </div>
         )
     }
